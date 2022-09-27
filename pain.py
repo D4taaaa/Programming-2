61e5062076b64adeb5ba2f4edc702ea0ad9da6bd
Ledus pārdevējs <63516519+D4taaaa@users.noreply.github.com>
2022-09-27 22:26:27 +0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- pain.py
+++ pain.py
@@ -1,3 +1,6 @@
+# Dāvis Karpovskis(pašlaik pārdomā izvēli iet tālāk programmēšanā)
+
+
 class Rekinatajs:
     def __init__(self):
         pass
@@ -6,25 +9,57 @@ class Rekinatajs:
         i = num
         AHHh = 1
         while i != 0:
-            AHHh = AHHh*i
-            i = i-1
+            AHHh = AHHh * i
+            i = i - 1
         return AHHh
 
+    def Sum(self, num):
+        i = num
+        AHHh = 0
+        while i != 0:
+            AHHh = AHHh + i
+            i = i - 1
+        return AHHh
 
-    def Sum(self):
-        pass
+    def testPrime(self, num):
+        test = False
+        if num > 1:
+            for i in range(2, num):
+                if (num % i) == 0:
+                    test = True
 
-    def testPrime(self):
-        pass
+                    break
 
-    def TableMult(self):
-        pass
+        if test:
+            return f"{num} is not a prime number😢"
+        else:
+            return f"{num} is a prime number🤓"
 
-    def listDiv(self):
-        pass
+    def TableMult(self, num):
+        for i in range(1, num + 1):
+            for j in range(1, num + 1):
+                print(i * j, end='\t')
+            print('')
+        return ''  # lai neraditu None pec tabulas
 
-    def listDivPrime(self):
-        pass
+    def listDiv(self, num):
+        for i in range(1, num + 1):
+            if num % i == 0:
+                print(i)
+        return ''
+
+    def listDivPrime(self, num):
+        c = 2
+        while (num > 1):
+
+            if (num % c == 0):
+                print(c, end=" ")
+                num = num / c
+            else:
+                c = c + 1
+        return ''
+
+
 
 f = Rekinatajs()
-print(f.factorial(10))
+print(f.listDivPrime(315))
