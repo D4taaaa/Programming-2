caf21b0908ae20fe26d8217b1b7a48621ca6fabe
Ledus pārdevējs <63516519+D4taaaa@users.noreply.github.com>
2022-09-27 14:42:27 +0300

    Create pain.py

---
+++ pain.py
@@ -0,0 +1,30 @@
+class Rekinatajs:
+    def __init__(self):
+        pass
+
+    def factorial(self, num):
+        i = num
+        AHHh = 1
+        while i != 0:
+            AHHh = AHHh*i
+            i = i-1
+        return AHHh
+
+
+    def Sum(self):
+        pass
+
+    def testPrime(self):
+        pass
+
+    def TableMult(self):
+        pass
+
+    def listDiv(self):
+        pass
+
+    def listDivPrime(self):
+        pass
+
+f = Rekinatajs()
+print(f.factorial(10))
